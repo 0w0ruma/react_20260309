10e89c9a8329c352e6bd254b5f72d6c25050bb1c
0w0ruma <0w0ruma@gmail.com>
2026-06-24 10:40:40 +0900

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,72 @@
 import React, { useState } from 'react'
+const headerNav = [
+
+  {
+    title: "intro",
+    url: "/",
+  },
+
+  {
+    title: "skill",
+    url: "/",
+  },
+
+  {
+    title: "site",
+    url: "/",
+  },
+
+  {
+    title: "port",
+    url: "/",
+  },
+
+  {
+    title: "contact",
+    url: "/",
+  },
+
+]
+
+
 
 const Header = () => {
 
   const [show, setShow] = useState(false);
-  // useState() : 컴퍼넌트의 상태를 관리해주는 훅
-  // 훅 : (=메서드)
-  // false : 초기값
-  // show(주인) : 상태를 만들고, 초기값 false로 지정
-  // setShow(노예) :  이 상태값을 바꾸는 함수(상태를 업그레이드 한 상태)
+  // show : false
 
+  const togglMenu = ()=>{
 
-  //들어온 값의 반대값으로 바꿔주는 함수
-  const toggleMenu = ()=>{
     setShow((pShow) => !pShow)
-    // ! : 반대값(참->거짓)(거짓->참)
+    // 들어온 입력값을 0 -> 1, 1 -> 0로 변경해준는 함수
+
+
   }
 
   return (
     <header id='header' role='banner'>
       <section className='header_inner'>
         <h1 className="header_logo">
-          <a href="/">portfolio <em>react.jsx</em></a>
+          <a href="/">portfolio <em>react.js</em></a>
         </h1>
         <nav className={`header_nav ${show ? "show" : ""}`} role='navigation' aria-label='800이상 메뉴'>
-
           <ul>
-            <li><a href="/">Intro</a></li>
-            <li><a href="/">Skill</a></li>
-            <li><a href="/">Site</a></li>
-            <li><a href="/">Port</a></li>
-            <li><a href="/">Contents</a></li>
+            {headerNav.map((nav,key)=>(
+               <li key={key}><a href={nav.url}>{nav.title}</a></li>
+            ))}
+           
+         
           </ul>
         </nav>
-        <nav className="header_nav_m" 
-        id='headerToggle' 
-        role='button' 
-        tabIndex={0}
-        onClick={toggleMenu}
+        <nav
+          className="header_nav_m"
+          id='headerToggle'
+          role='button'
+          tabIndex={0}
+          onClick={togglMenu}
+          // 클릭했을 때 toggleMenu 함수를 실행하라.
+          aria-expanded={show ? "true" : "false"}
+
         >
           <span></span>
         </nav>
